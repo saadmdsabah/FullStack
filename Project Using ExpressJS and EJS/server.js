beff88704b07d1a47736ced0067e7a7d60c11e12
Saad <saadmdsabahuddin@gmail.com>
2024-09-12 21:00:30 +0530

    Adding files for EJS

---
+++ Project Using ExpressJS and EJS/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const path = require("path")
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "public")))
+
+//! Home Page
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views", "home.html"))
+})
+
+//! about Page
+app.get("/about", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+})
+
+//! contact Page
+app.get("/contact", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views", "contact.html"));
+})
+
+//! gallery Page
+app.get("/gallery", (req, res)=>{
+    res.sendFile(path.join(__dirname, "views", "gallery.html"));
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
