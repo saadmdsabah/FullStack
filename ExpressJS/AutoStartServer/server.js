c5a09dae515b70f75ec9542beba8869c98b393bd
Saad <saadmdsabahuddin@gmail.com>
2024-09-12 08:57:25 +0530

    Adding ExpressJS

---
+++ ExpressJS/AutoStartServer/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+//!Initializing the ExpressJS
+const app = express();
+
+//!Creating port
+const PORT = 3000;
+
+//!creating Request Handler
+app.get("/", (req, res) => {
+  res.send("Hello, World htid is being added \n also this THIS IS US");
+});
+
+//!Starting the App
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
